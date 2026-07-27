03c06adbfdacd13692b3512d4e598d59ef98082a
Macho <nonsonnabugwu911@gmail.com>
2026-07-27 12:23:35 +0100

    started unit tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import User
 
-# Create your tests here.
+class RegisterTest(APITestCase):
+    def test_register(self):
+        data = {
+            'username': 'macho',
+            'email': 'macho@gmail.com',
+            'password': 'Macholina911#'
+        }
+
+        response = self.client.post('/api/auth/register/', data, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class LoginTest(APITestCase):
+    def setUp(self):
+        User.objects.create_user(
+            username = 'macho',
+            email = 'macho@gmail.com',
+            password = 'Macholina911#'
+        )
+    def test_login(self):
+        data = {
+                    'username': 'macho',
+                    'email': 'macho@gmail.com',
+                    'password': 'Macholina911#'
+                }
+        
+        response = self.client.post('/api/auth/login/', data, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)  
+        self.assertIn('refresh', response.data)  
+
+# add agent register and check role
